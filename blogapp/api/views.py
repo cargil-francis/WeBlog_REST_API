a63b0574b60f7f1c03495264d7c9a7d9e1d03e22
cargil-francis <kitty.f9876@gmail.com>
2023-03-23 02:08:29 +0530

    "user": UserSerializer(user).dataCommit: Update RegisterSerializer to include email field validation and password matching validation

--- blogapp/api/views.py
+++ blogapp/api/views.py
@@ -3,6 +3,7 @@ from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework_simplejwt.views import TokenObtainPairView
 from rest_framework import generics
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated,AllowAny,IsAdminUser
 from rest_framework.response import Response
 from rest_framework.generics import CreateAPIView
 from django.contrib.auth.models import User
@@ -33,13 +34,16 @@ class TokenBlacklistView(APIView):
 
  # User registeration
 
-class RegisterAPIView(generics.GenericAPIView):
+class RegisterAPIView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = (AllowAny,)
     serializer_class = RegisterSerializer
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
+         
 
         response_data = {
             "message": "User created successfully",
